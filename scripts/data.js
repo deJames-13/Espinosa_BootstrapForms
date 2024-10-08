0742858bca5d6b914200a023390a72f76f23b310
deJames-13 <ail>
2024-10-08 15:11:15 +0800

    s

---
+++ scripts/data.js
@@ -0,0 +1,75 @@
+export const data = [
+    {
+      "title": "San Agustin Church",
+      "description": "Constructed back in 1586, San Agustin Church is one of the oldest churches of the Philippines.",
+      "location": "General Luna St, Manila, 1002 Metro Manila, Philippines",
+      "timing": "8:00 AM to 12:00 PM and 1:00 PM to 6:00 PM",
+      "price": "Php 89 per head",
+      "image": "https://media2.thrillophilia.com/images/photos/000/367/429/original/1614169042_shutterstock_1076060522.jpg?w=753&h=450&dpr=1.0"
+    },
+    {
+      "title": "National Museum of the Philippines",
+      "description": "The National Museum of the Philippines is one of the best Philippines places to visit, if you want to explore and learn more about the culture, history, and tradition of the country.",
+      "location": "Location: P. Burgos Drive, Rizal Park, Manila, Philippines",
+      "timing": "Timings: 10 AM to 5 PM; Closed on Monday",
+      "price": "",
+      "image": "https://media2.thrillophilia.com/images/photos/000/367/430/original/1614169120_shutterstock_1123003349.jpg?w=753&h=450&dpr=1.0"
+    },
+    {
+      "title": "Banaue Rice Terraces",
+      "description": "The famous Banaue Rice Terraces once were just winding fields that laid embracing a mountain side. Although the locals had totally abandoned this place, young farmers at the present day are reviving these plantations.",
+      "location": "Location: Nueva Vizcaya - Ifugao - Mountain Province Rd, Banaue, Ifugao, Philippines",
+      "timing": "",
+      "price": "Price: 20 PHP per head",
+      "image": "https://media2.thrillophilia.com/images/photos/000/367/510/original/1614174738_1614149383_shutterstock_1462704626.jpg.jpg?w=753&h=450&dpr=1.0"
+    },
+    {
+      "title": "Puerto Princesa Subterranean River National Park ",
+      "description": "Lying just an hour’s drive away from the city center of Puerto Princesa,  the Puerto Princesa Subterranean River National Park is the abode of nature lovers. Also referred to as the Puerto Princesa Underground River, this national park had its name in the 2012 list of the New 7 Wonders of Nature. ",
+      "location": "Location: Puerto Princesa, MIMAROPA, Philippines",
+      "timing": "Timings: 8 AM to 4 PM",
+      "price": "Price: PHP 500 per head",
+      "image": "https://media2.thrillophilia.com/images/photos/000/367/509/original/1614174714_1614149228_shutterstock_489906367.jpg.jpg?w=753&h=450&dpr=1.0"
+    },
+    {
+      "title": " Siargao",
+      "description": "Siargao is known to be one of the famous places in the Philippines for surfing. Even though the island is very small, almost like a teardrop, it’s one of the most visited places.",
+      "location": "Location: Towards the Northeast coast of Mindanao, Philippines",
+      "timing": "",
+      "price": "",
+      "image": "https://media2.thrillophilia.com/images/photos/000/373/184/original/1621935231_shutterstock_1177486879.jpg?w=753&h=450&dpr=1.0"
+    },
+    {
+      "title": "Samal Island ",
+      "description": "Samal Island is said to be one of the largest resort islands in the entire Philippines. It's one of those places where you won't ever get bored, thanks to the joyous lifestyle and the exquisite scenarios surrounding the island.",
+      "location": "Location: Davao Gulf, Philippines",
+      "timing": "",
+      "price": "",
+      "image": "https://media2.thrillophilia.com/images/photos/000/373/182/original/1621935113_shutterstock_1830669002.jpg?w=753&h=450&dpr=1.0"
+    },
+    {
+      "title": "Coron Island ",
+      "description": "Being the third largest island, Coron is one of the best places to visit in the Philippines. Towards the centre of the island, you will find limestone peaks of several heights, with caves underneath which one can explore further to have a day full of adventures. ",
+      "location": "Location: 100 kilometres towards the north direction of Palawan, Philippines",
+      "timing": "",
+      "price": "",
+      "image": "https://media2.thrillophilia.com/images/photos/000/373/181/original/1621935059_shutterstock_225128794.jpg?w=753&h=450&dpr=1.0"
+    },
+    {
+      "title": "Tagaytay",
+      "description": "Out of all the tourist places in the Philippines, Tagaytay is quite popular in scenic beauty and the comfort its weather provides",
+      "location": "Location: Calabarzon region, Philippines",
+      "timing": "",
+      "price": "",
+      "image": "https://media2.thrillophilia.com/images/photos/000/373/185/original/1621935297_shutterstock_1384011518.jpg?w=753&h=450&dpr=1.0"
+    },
+    {
+      "title": "Rizal Park",
+      "description": "Known popularly for its historical significance, Rizan Park is the largest park in the city of Manila. Spread over an approximate area of around 140 acres, the park stands in the spot where Jose Rizal, the country’s national hero was once executed. At the present day, Rizal Park features lush gardens, beautiful musical fountains, an Orchidarium, and statues of famous historical personalities.",
+      "location": "Location:  Roxas Blvd, Malate, Manila, 1000 Metro Manila, Philippines",
+      "timing": "Timings: 5 AM to 9 PM",
+      "price": "Price: PHP 18 per head",
+      "image": "https://media2.thrillophilia.com/images/photos/000/367/447/original/1614170548_shutterstock_1019903074.jpg?w=753&h=450&dpr=1.0"
+    }
+  ];
+  
---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+import { data } from './data.js';
+
+// Function to generate cards based on data
+const generateCards = () => {
+    const container = document.getElementById('cards-container');
+
+    data.forEach(item => {
+        const card = `
+            <div class="col-md-6 mb-4">
+                <div class="card">
+                    <img src="${item.image}" class="card-img-top" alt="${item.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.title}</h5>
+                        <p>${item.description}</p>
+                        <p>${item.location}</p>
+                        <p>${item.timing}</p>
+                        <p>${item.price}</p>
+                    </div>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#inquiryModal">
+                        Send Inquiry 
+                    </button>
+                </div>
+            </div>
+        `;
+        container.innerHTML += card;
+    });
+};
+
+// Call the function to generate the cards
+generateCards();
