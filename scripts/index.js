2c83de28af931c6ef60674d0ca59bf52ca69d180
deJames-13 <ail>
2024-10-22 13:58:29 +0800

    asdf

--- scripts/index.js
+++ scripts/index.js
@@ -7,19 +7,19 @@ const generateCards = () => {
     data.forEach(item => {
         const card = `
             <div class="col-md-6 mb-4 container d-flex justify-content-center">
-                <div class="card">
-                    <img src="${item.image}" class="card-img-top" alt="${item.title}">
-                    <div class="card-body">
-                        <h5 class="card-title">${item.title}</h5>
-                        <p>${item.description}</p>
-                        <p>${item.location}</p>
-                        <p>${item.timing}</p>
-                        <p>${item.price}</p>
-                    </div>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#inquiryModal">
-                        Send Inquiry 
-                    </button>
+            <div class="card">
+                <img src="${item.image}" class="card-img-top" alt="${item.title}">
+                <div class="card-body" style="overflow-y: auto; max-height: 200px;">
+                <h5 class="card-title">${item.title}</h5>
+                <p>${item.description}</p>
+                <p><strong>Location:</strong> ${item.location}</p>
+                <p><strong>Timing:</strong> ${item.timing}</p>
+                <p><strong>Price:</strong> ${item.price}</p>
                 </div>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#inquiryModal">
+                Send Inquiry 
+                </button>
+            </div>
             </div>
         `;
         container.innerHTML += card;
